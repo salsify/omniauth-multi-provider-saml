9f9e393c0a48afdd6877673f4fb5ac884de97eda
Joel Turkel <jturkel@salsify.com>
2015-10-07 14:39:34 -0400

    Make identity provider id regex configurable.

--- lib/omniauth/saml/multi_provider/handler.rb
+++ lib/omniauth/saml/multi_provider/handler.rb
@@ -5,20 +5,20 @@ module OmniAuth
   module SAML
     module MultiProvider
       class Handler
-        UUID_REGEX = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+        attr_reader :path_prefix, :provider_name, :identity_provider_id_regex
 
-        attr_reader :path_prefix, :provider_name
-
-        def initialize(path_prefix: OmniAuth.config.path_prefix, provider_name: 'saml', &provider_options_generator)
+        def initialize(path_prefix: OmniAuth.config.path_prefix, provider_name: 'saml',
+                       identity_provider_id_regex: /\w+/, &identity_provider_options_generator)
           raise 'Missing provider options generator block' unless block_given?
 
           @path_prefix = path_prefix
           @provider_name = provider_name
-          @provider_options_generator = provider_options_generator
+          @identity_provider_id_regex
+          @identity_provider_options_generator = identity_provider_options_generator
 
           # Eagerly compute these since lazy evaluation will not be threadsafe
           @provider_path_prefix = "#{path_prefix}/#{provider_name}"
-          @saml_path_regex = /^#{@provider_path_prefix}\/(?<identity_provider_id>#{UUID_REGEX})/
+          @saml_path_regex = /^#{@provider_path_prefix}\/(?<identity_provider_id>#{identity_provider_id_regex})/
           @request_path_regex = /#{saml_path_regex}\/?$/
           @callback_path_regex = /#{saml_path_regex}\/callback\/?$/
         end
@@ -36,26 +36,26 @@ module OmniAuth
         private
 
         attr_reader :provider_path_prefix, :saml_path_regex, :request_path_regex, :callback_path_regex,
-                    :provider_options_generator
+                    :identity_provider_options_generator
 
         def setup(env)
-          identity_provider_uuid = extract_identity_provider_id(env)
-          if identity_provider_uuid
+          identity_provider_id = extract_identity_provider_id(env)
+          if identity_provider_id
             options = env['omniauth.strategy'].options
-            add_path_options(options, identity_provider_uuid)
-            add_identity_provider_options(options, identity_provider_uuid)
+            add_path_options(options, identity_provider_id)
+            add_identity_provider_options(options, identity_provider_id)
           end
         end
 
-        def add_path_options(options, identity_provider_uuid)
+        def add_path_options(options, identity_provider_id)
           options.merge!(
-              request_path: "#{provider_path_prefix}/#{identity_provider_uuid}",
-              callback_path: "#{provider_path_prefix}/#{identity_provider_uuid}/callback"
+              request_path: "#{provider_path_prefix}/#{identity_provider_id}",
+              callback_path: "#{provider_path_prefix}/#{identity_provider_id}/callback"
           )
         end
 
-        def add_identity_provider_options(options, identity_provider_uuid)
-          identity_provider_options = provider_options_generator.call(identity_provider_uuid) || {}
+        def add_identity_provider_options(options, identity_provider_id)
+          identity_provider_options = identity_provider_options_generator.call(identity_provider_id) || {}
           options.merge!(identity_provider_options)
         rescue e
           raise OmniAuth::Strategies::SAML::ValidationError.new('Invalid identity provider id')
